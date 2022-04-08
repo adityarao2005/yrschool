f7a3aae524db5d0bfba5a8609781000b1aeef0e8
Aditya Rao <aditya.g.rao@outlook.com>
2022-04-08 19:22:13 -0400

    Updated bug

--- YRSchool/src/main/java/com/yrhacks/apps/yrschool/App.java
+++ YRSchool/src/main/java/com/yrhacks/apps/yrschool/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("main"), 640, 480);
+        scene = new Scene(loadFXML("MainView"), 640, 480);
         stage.setScene(scene);
         stage.show();
     }
@@ -27,6 +27,7 @@ public class App extends Application {
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
+    	
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
