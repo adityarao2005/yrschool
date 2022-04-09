21286663951b2f4e8b42d2b4f2f4c3b1afdc1284
Aditya Rao <aditya.g.rao@outlook.com>
2022-04-09 14:41:54 -0400

    Updated UI and controllers

--- YRSchool/src/main/java/com/yrhacks/apps/yrschool/CourseController.java
+++ YRSchool/src/main/java/com/yrhacks/apps/yrschool/CourseController.java
@@ -1,10 +1,30 @@
 package com.yrhacks.apps.yrschool;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+import java.util.Collections;
 
 import javafx.fxml.FXML;
+import javafx.scene.layout.TilePane;
 
 public class CourseController {
+	@FXML
+	private TilePane tiles;
+
+	@FXML
+	private void initialize() throws IOException, URISyntaxException {
+		System.out.println(App.class.getProtectionDomain().getCodeSource().getLocation());
+		BufferedReader reader = new BufferedReader(new InputStreamReader(new File(new File(App.appDir.toURI()), "Courses").toURI().toURL().openStream()));
+
+		reader.lines().forEach((e) -> {
+			Tile tile = new Tile();
+			tile.setText(e);
+			tiles.getChildren().add(tile);
+		});
+	}
 
 	@FXML
 	public void moveToMain() throws IOException {
--- YRSchool/src/main/java/com/yrhacks/apps/yrschool/UnitController.java
+++ YRSchool/src/main/java/com/yrhacks/apps/yrschool/UnitController.java
@@ -5,7 +5,14 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 
 public class UnitController {
+	private String path;
 
+	@FXML
+	private void initialize() {
+		Object[] obj = App.getParams();
+		path = obj[0].toString();
+	}
+	
 	@FXML
 	public void moveToCourse() throws IOException {
 		App.setRoot("CourseView");
